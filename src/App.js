c943108be7b17e81e414d18d121cc845e3eb02d9
Alina <pavlenkoalina9694@gmail.com>
2021-10-26 22:29:15 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState([])
+    const [init, setInit] = useState('')
+    const [results, setResults] = useState(0)
+
+    useEffect(() => {
+        fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json")
+        .then((res) => res.json())
+        .then((data) => updateState(data));
+    }, [])
+    
+
+    const updateState = (data) => {
+        setData(data)
+       
+    }
+    const convertTo = (type) => {
+        const change = data.filter(item => item.cc === type);
+        const rate = change.map(item => item.rate);
+        const numbersRate = rate.map(Number);
+        const result = (init / numbersRate).toFixed(2);
+        setResults(result)
+    }
+  
+    return (
+      <div className="app-convert">
+          <input type='number'
+                className='convert-input'
+                placeholder='введите значение в гривнах'
+                value={init}
+                onChange={(e) => setInit(e.target.value)}/>
+        <div className="counter">{results}</div>
+        <div className="controls">
+          <button onClick={() => convertTo("USD")}>USD</button>
+          <button onClick={() => convertTo("EUR")}>EUR</button>
+          <button onClick={() => convertTo("PLN")}>PLN</button>
+          <button onClick={() => convertTo("RUB")}>RUB</button>
+        </div>
+      </div>
+  )
+    
 }
 
 export default App;
+
